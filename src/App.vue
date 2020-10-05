90de25926b0e8a2f92d4b7a67087a6a130b54000
dmitriyignatiev83 <dmitriy.ignatiev83@gmail.com>
2020-10-05 22:30:39 +0400

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,143 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container mt-5">
+    <div class="row">
+      <div class="col form-inline">
+        <b-form-input v-model="newTask" placeholder="Enter Task" @keyup.enter="add"></b-form-input>
+        <b-button class="ml-2" variant="primary" @click="add">Add</b-button>
+      </div>
+    </div>
+    <div v-for="edge in allUsers.edges" :key="edge.node.id">{{edge.node.name}}</div>
+    <input type="text"  v-model="newTask" @keyup.enter="onClicked" />
+    <button @click="onClicked">Add User</button>
+
+    <div class="row mt-3">
+      <div class="col-md-3">
+        <div class="p-2 alert alert-secondary">
+          <h3>Backlog</h3>
+          <draggable class="list-group kanban-column" :list="arrBacklog" group="task">
+            <div class="list-group-item" v-for="element in arrBacklog" :key="element.name">
+              {{element.name}}
+            </div>
+          </draggable>
+        </div>
+      </div>
+
+
+      <div class="col-md-3">
+        <div class="p-2 alert alert-primary">
+          <h3>Progress</h3>
+          <draggable class="list-group kanban-column" :list="arrInprogress" group="task">
+            <div class="list-group-item" v-for="element in arrInprogress" :key="element.name">
+              {{element.name}}
+            </div>
+          </draggable>
+        </div>
+      </div>
+
+
+
+      <div class="col-md-3">
+        <div class="p-2 alert alert-warning">
+          <h3>Tested</h3>
+          <draggable class="list-group kanban-column" :list="arrTested" group="task">
+            <div class="list-group-item" v-for="element in arrTested" :key="element.name">
+              {{element.name}}
+            </div>
+          </draggable>
+        </div>
+      </div>
+
+
+      <div class="col-md-3">
+        <div class="p-2 alert alert-success">
+          <h3>Done</h3>
+          <draggable class="list-group kanban-column" :list="arrDone" group="task">
+            <div class="list-group-item" v-for="element in arrDone" :key="element.name">
+              {{element.name}}
+            </div>
+          </draggable>
+        </div>
+      </div>
+
+
+
+    </div>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import draggable from "vuedraggable";
+// import { useMutation } from '@vue/apollo-composable'
+import gql from 'graphql-tag';
+
+
+const createU = gql`
+    mutation createUser($name:String){
+     createUser(name: $name){
+     ok
+     }
+    }
+
+
+`
 
 export default {
   name: 'App',
+  apollo: {
+    allUsers: gql`query {
+            allUsers{
+            edges{
+              node{
+                id
+                name
+              }
+            }
+           }
+          }`,
+  },
   components: {
-    HelloWorld
-  }
-}
+    draggable
+  },
+  data(){
+    return{
+      newTask:"",
+      arrBacklog:[
+        {name:'Code refactoring1'},
+        {name:'Code refactoring2'},
+        {name:'Code refactoring3'},
+        {name:'Code refactoring4'}
+      ],
+      arrInprogress:[],
+      arrTested:[],
+      arrDone:[],
+    }
+  },
+  methods: {
+    add() {
+      if (this.newTask) {
+        this.arrBacklog.push({name: this.newTask});
+        this.newTask = "";
+      }
+    },
+
+     onClicked() {
+      console.log('Hi')
+       this.$apollo.mutate({
+        mutation: createU,
+         variables:{
+          name: this.name
+         },
+
+
+      }).then(() => {console.log('Done', this.name)})
+    },
+  }}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.kanban-column{
+  min-height: 300px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,44 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+
+import VueApollo from 'vue-apollo'
+
+// Import Bootstrap Vue
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
 
+const httpLink = new HttpLink({
+  // You should use an absolute URL here
+  uri: 'http://localhost:5000/graphql'
+})
+
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+  connectToDevTools: true
+})
+
+Vue.use(VueApollo)
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+  defaultOptions: {
+    $loadingKey: 'loading'
+  }
+})
+
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+
 new Vue({
+  provide: apolloProvider.provide(),
   render: h => h(App),
 }).$mount('#app')
